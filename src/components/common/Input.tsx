98bb6ee67141ee8b49cac7677710eaf9559e4f71
imkrmin <zum2n2@gmail.com>
2025-01-06 00:18:05 +0900

    Feat: Input 공통 컴포넌트 구현 및 react-hook-form 설치

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,29 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
+type TInputProps = React.ComponentProps<"input"> & {
+  title?: string;
+  name: string;
+  addStyle?: string;
+  register?: UseFormRegisterReturn;
+  children?: React.ReactNode;
+};
+
+const Input = ({ title, name, addStyle, register, children, ...restProps }: TInputProps) => {
+  return (
+    <div className="flex flex-col w-full gap-1">
+      {title && (
+        <label htmlFor={name} className="text-[16px] font-medium">
+          {title}
+        </label>
+      )}
+      <input
+        {...register}
+        className={`${addStyle} h-[50px] rounded-[10px] px-4 py-3 border border-gray-400 focus:border-gray-600`}
+        {...restProps}>
+        {children}
+      </input>
+    </div>
+  );
+};
+
+export default Input;
